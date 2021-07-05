791b1589958e501236c9ed99eb406cbe9e9f2f62
Alex <alar0@yahoo.com>
2021-07-05 23:15:26 +0300

    tooltip in footer

--- js/data-page.js
+++ js/data-page.js
@@ -5,3 +5,12 @@ theme.coolUpdate();
 window.addEventListener('storage', (e) => {
     theme.coolUpdate();
 })
+
+$(function(){
+    var includes = $('[data-include]');
+    //document.querySelector("[data-include]").innerText;
+    jQuery.each(includes, function(){
+        var file = 'components/' + $(this).data('include') + '.html';
+        $(this).load(file);
+    });
+});
