02b7c98912b5b2e1102449e8d7951f32aa779457
Alex <alar0@yahoo.com>
2021-07-06 08:41:19 +0300

    fetch personages, add personages to data.html

--- js/data-page.js
+++ js/data-page.js
@@ -14,3 +14,44 @@ $(function(){
         $(this).load(file);
     });
 });
+
+
+let peoples = [];
+let cupPeoples = [];
+let nextPeoplesPage;
+let prevPeoplesPage;
+function Personage(pers) {
+    return `<div class="personage-item_container" id="pers_${pers.i}" style="flex: 1; transform: translateY(70%)">
+        <h5 style="margin: 0 auto">${pers.name}</h5>
+        <p>Родился в ${pers.birth_year}</p>    
+    </div>`;
+}
+
+function drawPersonages(startIndex=0, endIndex=3) {
+    cupPeoples = peoples.slice(startIndex, endIndex);
+
+    const HTMLPeoples = cupPeoples.map(el => Personage(el));
+
+    $('.personages_container')[0].innerHTML = HTMLPeoples.join('');
+
+    $('.personage-item_container').on('click', function() {
+        if (this.style.transform === "translate(0%, 10%) matrix(1, 0, 0, 1, 0, 0)") {
+            TweenMax.to(this, 0.6, {y:'70%', ease: Power4.easeOut});
+        } else {
+            TweenMax.to(this, 0.6, {y:'10%', ease: Back.easeOut});
+        }
+    });
+}
+
+window.onload = async function () {
+    let res = await fetch("https://swapi.dev/api/people").then(res => res.json());
+    peoples = res.results.map( (el, i) => {
+        el.i = i;
+        return el;
+    });
+
+    nextPeoplesPage = res.next;
+    prevPeoplesPage = res.previous;
+
+    drawPersonages();
+};
